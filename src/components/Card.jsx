81bc3c0412c5760b9fa74aeedcb850724d6fdd61
gokulhansv <gokulhansv@gmail.com>
2022-12-12 12:20:33 +0530

    new

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card(data) {
                 {/* <img src="https://source.unsplash.com/random/300x300/?2" alt="" class="object-cover object-center w-full rounded-t-md h-72 bg-gray-500" /> */}
                 <div class="flex flex-col justify-between p-6 space-y-8">
                     <div class="space-y-2">
-                        <h2 class="text-3xl font-semibold tracking-wide">lectus {data}</h2>
+                        <h2 class="text-3xl font-semibold tracking-wide">lectus </h2>
                         <p class="text-gray-800">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
                     </div>
                     <button type="button" class="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Click here for More Details</button>
