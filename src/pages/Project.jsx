bea5aa5cf1a929dbe5f3eeb19ebb3ac88c365a49
gokulhansv <gokulhansv@gmail.com>
2022-12-12 15:28:18 +0530

    new

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,9 +3,54 @@ import React from 'react'
 function Project() {
     return (
         <>
+            <h2 class="mb-8 text-3xl md:text-4xl font-bold leading-none text-center text-green-600">Blogging App</h2>
 
-            <div class="px-4 pb-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 text-gray-800">
-                <h2 class="mb-8 text-3xl md:text-4xl font-bold leading-none text-center">What do we have to offer?</h2>
+            <h2 class="my-8 mt-12 ml-5 text-2xl md:text-3xl font-bold leading-none md:ml-16">Demo Video</h2>
+
+            <div className='mt-3 flex items-center justify-center'>
+                <iframe className='w-screen h-60 md:w-2/4 md:h-96 ' src="https://www.youtube.com/embed/9qoH0Ur9mAU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div class="px-4 pb-5 mb-24 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10 text-gray-800">
+ 
+                <h2 class="py-6 pt-8 text-2xl md:text-3xl font-bold leading-none">Technologies Used</h2>
+
+                <ul class="grid gap-3 md:grid-cols-2 lg:grid-cols-3">
+                    <li class="flex items-center space-x-2">
+                        <span>React JS</span>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                        <span>My SQL</span>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                        <span>Node Js</span>
+                    </li>
+                </ul>
+
+                <h2 class="py-6 text-2xl md:text-3xl font-bold leading-none">Features </h2>
+
+                <ul class="grid gap-3 md:grid-cols-2 lg:grid-cols-3">
+                    <li class="flex items-center space-x-2">
+                        <span>Write Blog</span>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                        <span>Authentication</span>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                        <span>Crud Operation on Blog</span>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                        <span>Login/SignUp Functions</span>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                        <span>Professional Text Area</span>
+                    </li>
+                </ul>
+
+
+
+                <h2 class="py-8 text-2xl md:text-3xl font-bold leading-none">Bundle Contain</h2>
+
                 <ul class="grid gap-3 md:grid-cols-2 lg:grid-cols-3">
                     <li class="flex items-center space-x-2">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 fill-current text-green-600">
@@ -50,14 +95,12 @@ function Project() {
                         <span>Customisable for your need </span>
                     </li>
                 </ul>
-            </div>
 
-            <div className=''>
-                <iframe className='w-screen h-60 md:w-full md:h-full' src="https://www.youtube.com/embed/9qoH0Ur9mAU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
             </div>
 
             <div className="text-xl fixed bottom-5 right-5 left-5 bg-green-600 text-white rounded-md z-20">
-                <a href='https://wa.me/message/5XWWJKY6UH5EG1' class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-green-600 text-gray-50">Book Project Now</a>
+                <a href='https://wa.me/message/5XWWJKY6UH5EG1' class="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-green-600 text-gray-50">Book This Project Now</a>
             </div>
         </>
     )
