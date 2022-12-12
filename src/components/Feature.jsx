0a22dc3b36044116087400ba64d293f67d90da5e
gokulhansv <gokulhansv@gmail.com>
2022-12-12 22:30:38 +0530

    new

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -11,7 +11,7 @@ function Feature() {
                     <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Starting From</h5>
                     <div class="flex items-baseline text-gray-900 dark:text-white">
                         {/* <span class="text-4xl font-semibold">₹</span> */}
-                        <span class="text-5xl font-extrabold tracking-tight">₹ 1000</span>
+                        <span class="text-5xl font-extrabold tracking-tight">₹ 3000</span>
                         {/* <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">Starting From</span> */}
                     </div>
                     {/* <!-- List --> */}
